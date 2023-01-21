9f708067e17da4df34b5e13c565e4237143d553a
AlexandrTarabanko <116266583+AlexandrTarabanko@users.noreply.github.com>
2023-01-21 10:02:57 +0000

    Deploying to gh-pages from @ AlexandrTarabanko/parcel-blended-4@4aa6d5c01a3482d6bba21a3f6485dd091cb3dd47 🚀

---
+++ index.46ac495e.js
@@ -0,0 +1,2 @@
+$(".team-slide").slick({prevArrow:'<button type="button" class="my-slick-prev">&xlarr;</button>',nextArrow:'<button type="button" class="my-slick-next">&xrarr;</button>',infinite:!0,speed:300,slidesToScroll:1,adaptiveHeight:!0,slidesToShow:3,responsive:[{breakpoint:768,settings:{slidesToShow:2}}]});
+//# sourceMappingURL=index.46ac495e.js.map
